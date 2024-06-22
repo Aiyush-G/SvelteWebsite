c86fc59bce77f387bb97ceddba3152529b33866a
Aiyush Gupta <agupta17@agsb.co.uk>
2024-06-22 17:59:42 +0100

    understading why arrays are reactive

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <script>
     let count = 0;
+    let numbers = [1,2,3,4,5,6];
+
+
     $: doubled = count*2;
-    $: console.log(`the count is ${count}`);
+    // $: console.log(`the count is ${count}`);
+    $: console.log(`${numbers}`);
 
     $: if (count >= 20){
         alert("THE COUNT IS SUPER HIGH, gonna reset it now");
@@ -15,6 +19,11 @@
     function reset(){
         count = 0;
     }
+
+    function addNumber(){
+        numbers = [...numbers, numbers.length + 1];
+        
+    }
 </script>
 
 <h1>Learning Svelte!</h1>
@@ -22,8 +31,10 @@
 <button on:click={increment}>
     Clicked {count}
 </button>
+
 <button on:click={reset}>
     Reset the count to 0 
 </button>
-
 <p>Count Doubled is {doubled}</p>
+
+<button on:click={addNumber}>Add a Number!!</button>
