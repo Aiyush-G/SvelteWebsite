116f5abbeeaf741cd45123e1cf457b0a615850c4
Aiyush Gupta <agupta17@agsb.co.uk>
2024-06-22 20:30:39 +0100

    Experimenting with component dispatch events....

---
+++ myapp/src/routes/Form.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+  
+    const dispatch = createEventDispatcher();
+  
+    let name = '';
+    let email = '';
+  
+    /**
+     * @param {{ preventDefault: () => void; }} event
+     */
+    function handleSubmit(event) {
+      event.preventDefault();
+      dispatch('submit', { name, email });
+    }
+  </script>
+  
+  <form on:submit={handleSubmit}>
+    <label>
+      Name:
+      <input type="text" bind:value={name} />
+    </label>
+    <br />
+    <label>
+      Email:
+      <input type="email" bind:value={email} />
+    </label>
+    <br />
+    <button type="submit">Submit</button>
+  </form>
+  
---
+++ myapp/src/routes/FormParent.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Form from './Form.svelte';
+  
+    /**
+     * @param {{ detail: { name: any; email: any; }; }} event
+     */
+    function handleFormSubmit(event) {
+      const { name, email } = event.detail;
+      console.log('Form submitted with:', { name, email });
+      // You can perform any action with the form data here, such as sending it to a server
+    }
+  </script>
+  
+  <Form on:submit={handleFormSubmit} />
+  
+  
