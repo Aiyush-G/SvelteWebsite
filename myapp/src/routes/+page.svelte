e297d52582001225bbdc1350f6e358ad94c2cd45
Aiyush Gupta <agupta17@agsb.co.uk>
2024-06-22 17:52:17 +0100

    separate reset count button added

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -11,12 +11,19 @@
     function increment(){
         count+= 1;
     }
+
+    function reset(){
+        count = 0;
+    }
 </script>
 
 <h1>Learning Svelte!</h1>
 
 <button on:click={increment}>
     Clicked {count}
+</button>
+<button on:click={reset}>
+    Reset the count to 0 
 </button>
 
 <p>Count Doubled is {doubled}</p>
